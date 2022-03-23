3d69a797920e4b0dfc3585c3e16699cd7e70e272
Kabir Kanjani <andrewkabir@gmail.com>
2022-03-23 16:00:51 +0530

    Models Pushed

---
+++ server/model/admin.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const admin = new mongoose.Schema({
+    admin_id:{
+        type:String,
+        required:true,
+    },
+    admin_name:{
+        type:String,
+        required:true,
+    },
+    admin_password:{
+        type:String,
+        required:true,
+    },
+    admin_email:{
+        type:String,
+        required:true,
+    },
+    admin_contact:{
+        type:Number,
+        required:true,
+    },
+});
+
+module.exports = mongoose.model("admin",admin);
