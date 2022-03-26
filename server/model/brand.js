fd55253952dbe8fee056c5538a2a78ba30011f37
RenishGadhiya <89795005+RenishGadhiya@users.noreply.github.com>
2022-03-26 14:00:07 +0530

    for pulling from main

--- server/model/brand.js
+++ server/model/brand.js
@@ -1,22 +1,18 @@
 const mongoose = require("mongoose");
 
 const brand = new mongoose.Schema({
-    brand_id:{
-        type:String,
-        required:true
-    },
-    brand_name:{
-        type:String,
-        required:true
-    },
-    brand_dor:{
-        type:Date,
-        required:true
-    },
-    brand_status:{
-        type:Number,    // 0 : unverified	1 : verified	-1 : block
-        required:true
-    }
+  brand_name: {
+    type: String,
+    required: true,
+  },
+  brand_dor: {
+    type: Date,
+    required: true,
+  },
+  brand_status: {
+    type: Number, // 0 : unverified	1 : verified	-1 : block
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("BRAND", brand);
